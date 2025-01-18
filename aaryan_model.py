cd3a8fdae1b844318069fe6c57c82863a658f4ed
Aaryan Tomar <aaryantomar12@gmail.com>
2025-01-18 13:31:28 -0500

    get relevant layers from model

--- aaryan_model.py
+++ aaryan_model.py
@@ -18,18 +18,6 @@ class model(nn.Module):
         self.fusionModel = None
         self.fcModel = None
 
-        self.createVisualModel()
-        self.createAudioModel()
-        self.createFusionModel()
-        self.createFCModel()
-
-        self.visualModel = self.visualModel.cuda()
-        self.audioModel = self.audioModel.cuda()
-        self.fcModel = self.fcModel.cuda()
-
-        self.optim = torch.optim.Adam(self.parameters(), lr=lr)
-        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optim, step_size=1, gamma=lrDecay)
-
         self.loss_fn = nn.CrossEntropyLoss()
 
     def createVisualModel(self):
@@ -79,131 +67,28 @@ class model(nn.Module):
             nn.ReLU(),
             nn.Linear(128, 2)
         )
-
-    def train_network(self, loader, epoch, **kwargs):
-
-        self.train()
-        self.scheduler.step(epoch - 1)
-        lr = self.optim.param_groups[0]['lr']
-        index, top1, loss = 0, 0, 0
-        for num, (audioFeatures, visualFeatures, labels) in enumerate(loader, start=1):
-            self.zero_grad()
-
-            audioFeatures = torch.unsqueeze(audioFeatures, dim=1)
-
-            audioFeatures = audioFeatures.cuda()
-            visualFeatures = visualFeatures.cuda()
-            labels = labels.squeeze().cuda()
-
-            audioEmbed = self.audioModel(audioFeatures)
-            visualEmbed = self.visualModel(visualFeatures)
-
-            avfusion = torch.cat((audioEmbed, visualEmbed), dim=1)
-
-            fcOutput = self.fcModel(avfusion)
-
-            nloss = self.loss_fn(fcOutput, labels)
-
-            self.optim.zero_grad()
-            nloss.backward()
-            self.optim.step()
-
-            loss += nloss.detach().cpu().numpy()
-
-            top1 += (fcOutput.argmax(1) == labels).type(torch.float).sum().item()
-            index += len(labels)
-            sys.stderr.write(time.strftime("%m-%d %H:%M:%S") + \
-                             " [%2d] Lr: %5f, Training: %.2f%%, " % (epoch, lr, 100 * (num / loader.__len__())) + \
-                             " Loss: %.5f, ACC: %2.2f%% \\r" % (loss / (num), 100 * (top1 / index)))
-            sys.stderr.flush()
-        sys.stdout.write("\\n")
-
-        return loss / num, lr
-
-    def evaluate_network(self, loader, **kwargs):
-        self.eval()
-        predScores = []
-
-        loss, top1, index, numBatches = 0, 0, 0, 0
-
-        for audioFeatures, visualFeatures, labels in tqdm.tqdm(loader):
-            audioFeatures = torch.unsqueeze(audioFeatures, dim=1)
-            audioFeatures = audioFeatures.cuda()
-            visualFeatures = visualFeatures.cuda()
-            labels = labels.squeeze().cuda()
-
-            with torch.no_grad():
-                audioEmbed = self.audioModel(audioFeatures)
-                visualEmbed = self.visualModel(visualFeatures)
-
-                avfusion = torch.cat((audioEmbed, visualEmbed), dim=1)
-
-                fcOutput = self.fcModel(avfusion)
-
-                nloss = self.loss_fn(fcOutput, labels)
-
-                loss += nloss.detach().cpu().numpy()
-                top1 += (fcOutput.argmax(1) == labels).type(torch.float).sum().item()
-                index += len(labels)
-                numBatches += 1
-
-        print('eval loss ', loss / numBatches)
-        print('eval accuracy ', top1 / index)
-
-        return top1 / index
-
-    def saveParameters(self, path):
-        torch.save(self.state_dict(), path)
-
-    def loadParameters(self, path):
-        selfState = self.state_dict()
-        loadedState = torch.load(path)
-        for name, param in loadedState.items():
-            origName = name
-            if name not in selfState:
-                name = name.replace("module.", "")
-                if name not in selfState:
-                    print("%s is not in the model." % origName)
-                    continue
-            if selfState[name].size() != loadedState[origName].size():
-                sys.stderr.write("Wrong parameter length: %s, model: %s, loaded: %s" % (
-                origName, selfState[name].size(), loadedState[origName].size()))
-                continue
-            selfState[name].copy_(param)
 """
 
 tree = ast.parse(source_code)
 
-# Step 2: Extract class definitions
-class ModelAnalyzer(ast.NodeVisitor):
-    def __init__(self):
-        self.classes = []
-
+class ArgVisitor(ast.NodeVisitor):
     def visit_ClassDef(self, node):
-        class_info = {"name": node.name, "methods": [], "inherits": []}
-
-        # Extract base classes (e.g., nn.Module)
-        for base in node.bases:
-            if isinstance(base, ast.Name):
-                class_info["inherits"].append(base.id)
-            elif isinstance(base, ast.Attribute):
-                class_info["inherits"].append(f"{base.value.id}.{base.attr}")
-
-        # Extract methods within the class
-        for body_item in node.body:
-            if isinstance(body_item, ast.FunctionDef):
-                class_info["methods"].append(body_item.name)
-
-        self.classes.append(class_info)
+        # print(f"Class Name: {node.name}")
         self.generic_visit(node)
 
-# Step 3: Analyze the AST
-analyzer = ModelAnalyzer()
-analyzer.visit(tree)
+    def visit_FunctionDef(self, node):
+        # print(f"  Function Name: {node.name}")
+        
+        self.generic_visit(node)
 
-# Step 4: Print extracted class information
-for cls in analyzer.classes:
-    print(f"Class: {cls['name']}")
-    print(f"Inherits from: {', '.join(cls['inherits'])}")
-    print(f"Methods: {', '.join(cls['methods'])}")
-    print()
+    def visit_Call(self, node):
+        # if isinstance(node.func, ast.Name):
+        #     print(f"    Function Call: {node.func.id}")
+        if isinstance(node.func, ast.Attribute):
+            print(f"    Method Call: {node.func.attr}")
+        self.generic_visit(node) 
+
+
+# Step 3: Analyze the AST
+visitor = ArgVisitor()
+visitor.visit(tree)
