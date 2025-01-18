5b5ec086f856cb88e2803f1ac56713369864344a
lkang172 <kangzundao@gmail.com>
2025-01-18 09:27:49 -0500

    install highlightjs and create codeinput

---
+++ frontend/components/CodeInput.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import hljs from "highlight.js";
+
+const CodeInput = () => {
+  <>
+    <p>
+      <pre>
+        <code className="language-html"></code>
+      </pre>
+    </p>
+  </>;
+};
+
+export default CodeInput;
