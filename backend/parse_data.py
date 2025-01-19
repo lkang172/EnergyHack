3e6033f30296b13dc918a84982f2c2f867a662b1
Aaryan Tomar <aaryantomar12@gmail.com>
2025-01-18 19:04:34 -0500

    update

--- backend/parse_data.py
+++ backend/parse_data.py
@@ -1,54 +1,54 @@
 import ast
 
-source_code = '''self.loss_fn = nn.CrossEntropyLoss()
+source_code = """
+self.loss_fn = nn.CrossEntropyLoss()
+def createVisualModel(self):
+    self.visualModel = nn.Sequential(
+        nn.Conv2d(1, 64, 3, padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(64),
+        nn.MaxPool2d(2),
+        nn.Conv2d(64, 128, 3, padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(128),
+        nn.MaxPool2d(2),
+        nn.Conv2d(128, 128, 3, padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(128),
+        nn.MaxPool2d(2),
+        nn.Conv2d(128, 128, 3, padding=1),
+        nn.Flatten()
+    )
 
-    def createVisualModel(self):
-        self.visualModel = nn.Sequential(
-            nn.Conv2d(1, 64, 3, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(64),
-            nn.MaxPool2d(2),
-            nn.Conv2d(64, 128, 3, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(128),
-            nn.MaxPool2d(2),
-            nn.Conv2d(128, 128, 3, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(128),
-            nn.MaxPool2d(2),
-            nn.Conv2d(128, 128, 3, padding=1),
-            nn.Flatten()
-        )
+def createAudioModel(self):
+    self.audioModel = nn.Sequential(
+        nn.Conv2d(1, 64, 3, padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(64),
+        nn.MaxPool2d(2),
+        nn.Conv2d(64, 128, 3, padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(128),
+        nn.MaxPool2d(2),
+        nn.Conv2d(128, 128, 3, padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(128),
+        nn.MaxPool2d(2),
+        nn.Conv2d(128, 128, 3, padding=1),
+        nn.Flatten()
+    )
 
-    def createAudioModel(self):
-        self.audioModel = nn.Sequential(
-            nn.Conv2d(1, 64, 3, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(64),
-            nn.MaxPool2d(2),
-            nn.Conv2d(64, 128, 3, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(128),
-            nn.MaxPool2d(2),
-            nn.Conv2d(128, 128, 3, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(128),
-            nn.MaxPool2d(2),
-            nn.Conv2d(128, 128, 3, padding=1),
-            nn.Flatten()
-        )
+def createFusionModel(self):
+    pass
 
-    def createFusionModel(self):
-        pass
-
-    def createFCModel(self):
-        self.fcModel = nn.Sequential(
-            nn.Linear(29824, 1024),
-            nn.ReLU(),
-            nn.Linear(1024, 128),
-            nn.ReLU(),
-            nn.Linear(128, 2)
-        )
+def createFCModel(self):
+    self.fcModel = nn.Sequential(
+        nn.Linear(29824, 1024),
+        nn.ReLU(),
+        nn.Linear(1024, 128),
+        nn.ReLU(),
+        nn.Linear(128, 2)
+    )
 """
     
 
